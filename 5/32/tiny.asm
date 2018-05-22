092a13fdc84e5c4aba352615ff21e0ae78a5c927
PoroCYon <pcy@national.shitposting.agency>
2018-05-22 15:49:25 +0200

    fix 64-bit binary, crunch down to 944 bytes (using lzma1), add 32-bit variant (943 bytes)

---
+++ 5/32/tiny.asm
@@ -0,0 +1,46 @@
+
+  bits 32
+
+%define SYS_pause 29
+
+  org 0x00200000
+
+ehdr:
+  db  0x7F, "ELF"       ; e_ident
+  db  1                 ; EI_CLASS
+  db  1                 ; EI_DATA
+  db  1                 ; EI_VERSION
+  db  0                 ; EI_OSABI
+
+_start:
+  mov al, SYS_pause
+  int 0x80
+  ; jmp short _start    ; loop infinitely. if uncommented, change the 3 into
+                        ; a 2 in the "times 3..." line
+
+  int3                  ; exit without segfaulting
+  times 3 db 0
+
+  dw  2                 ; e_type
+  dw  3                 ; e_machine
+  dd  1                 ; e_version
+  dd  _start            ; e_entry
+  dd  phdr - ehdr       ; e_phoff
+phdr:
+  dd  1                 ; e_shoff       ; p_type
+  dd  0                 ; e_flags       ; p_offset
+  dd  ehdr              ; e_ehsize      ; p_vaddr
+                        ; e_phentsize
+  dw  1                 ; e_phnum       ; p_paddr
+  dw  0                 ; e_shentsize
+  dd  filesize          ; e_shnum       ; p_filesz
+                        ; e_shstrndx
+  dd  filesize                          ; p_memsz
+  db  5                                 ; p_flags
+  times 3 db 0
+  dd  0                                 ; p_align
+
+END:
+
+filesize equ END - ehdr
+
--- 5/tiny.asm
+++ 5/tiny.asm
@@ -1,37 +1,46 @@
-BITS 64
-  org 0x400000
- 
-ehdr:           ; Elf64_Ehdr
-  db 0x7f, "ELF", 2, 1, 1, 0 ; e_ident
-  times 8 db 0
-  dw  2         ; e_type
-  dw  0x3e      ; e_machine
-  dd  1         ; e_version
-  dq  _start    ; e_entry
-  dq  phdr - $$ ; e_phoff
-  dq  0         ; e_shoff
-  dd  0         ; e_flags
-  dw  ehdrsize  ; e_ehsize
-  dw  phdrsize  ; e_phentsize
-  dw  1         ; e_phnum
-  dw  0         ; e_shentsize
-  dw  0         ; e_shnum
-  dw  0         ; e_shstrndx
-  ehdrsize  equ  $ - ehdr
- 
-phdr:           ; Elf64_Phdr
-  dd  1         ; p_type
-  dd  5         ; p_flags
-  dq  0         ; p_offset
-  dq  $$        ; p_vaddr
-  dq  $$        ; p_paddr
-  dq  filesize  ; p_filesz
-  dq  filesize  ; p_memsz
-  dq  0x1000    ; p_align
-  phdrsize  equ  $ - phdr
- 
+
+  bits 64
+
+%define SYS_pause 34
+
+  org 0x100000000
+
+ehdr:                   ; Elf64_Ehdr
+  db  0x7f, "ELF"       ; e_ident   EI_MAG
+  db  2                 ;           EI_CLASS
+  db  1                 ;           EI_DATA
+  db  1                 ;           EI_VERSION
+  db  0                 ;           EI_OSABI
 _start:
-  mov eax, 29
-  int 0x80
- 
-filesize  equ  $ - $$
+  add rax, SYS_pause    ; padding
+  syscall
+  ; jmp short _start    ; loop infinitely. if uncommented, remove the
+                        ; 'db 0' under the int3
+  int3                  ; exit without segfaulting
+  db  0
+
+  dw  2                 ; e_type
+  dw  0x3e              ; e_machine
+  dd  1                 ; e_version
+  dd  _start - ehdr     ; e_entry (lo)
+phdr:
+  dd  1                 ; e_entry (hi)  ; p_type
+  dd  phdr - ehdr       ; e_phoff (lo)  ; p_flags
+  dd  0                 ; e_phoff (hi)  ; p_offset (lo)
+  dd  0                 ; e_shoff (lo)  ; p_offset (hi)
+  dq  ehdr              ; e_shoff (hi)  ; p_vaddr
+                        ; e_flags
+  dw  0x40              ; e_ehsize      ; p_paddr (0)
+  dw  phdr.end - phdr   ; e_phentsize   ;         (1)
+  dw  1                 ; e_phnum       ;         (2)
+  dw  0                 ; e_shentsize   ;         (3)
+  dq  filesize          ; e_shnum       ; p_filesz
+                        ; e_shstrndx
+  dq  filesize                          ; p_memsz
+  dq  0                                 ; p_align
+.end:
+
+END:
+
+filesize  equ END - ehdr
+
